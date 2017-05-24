79f7e1b75a9ad3bf008ff9c25ca8d2f710c581fa
SlyWink <slydev@openaliasbox.org>
2017-05-25 00:52:54 +0200

    Double thermistor driven PWM fan with max speed limited by pot

--- bedroomfan.c
+++ bedroomfan.c
@@ -30,11 +30,6 @@
 
 
 void Fan_Speed(uint8_t p_pwm) {
-
-// < 0xC0 pour tomber < 1,5A
-// = 0x50 fonctionnement normal
-// ~ 0x20 fonctionnement silencieux
-
   OCR0A = (p_pwm < PWM_MAX) ? p_pwm : PWM_MAX ;
 }
 
@@ -51,9 +46,8 @@ uint8_t ADC_Read(uint8_t p_mux) {
   uint16_t l_sum ;
   uint8_t l_count ;
 
-  // Clear MUX1 & MUX0 in ADMUX
-  ADMUX &= ~(_BV(MUX1)|_BV(MUX0)) ;
-  ADMUX |= p_mux ;
+  ADMUX &= ~(_BV(MUX1)|_BV(MUX0)) ; // Clear MUX1 & MUX0 in ADMUX
+  ADMUX |= p_mux ; // Select MUX pin
   l_sum = 0 ;
   // Read ADC several times
   for (l_count=0 ; l_count<ADC_READ_COUNT ; l_count++) {
@@ -66,9 +60,12 @@ uint8_t ADC_Read(uint8_t p_mux) {
 
 
 int main(void) {
-  uint8_t l_pot ;
-  uint8_t l_prev ;
-  uint8_t l_pwm ;
+//  uint8_t l_pot ;
+//  uint8_t l_prev ;
+//  uint8_t l_pwm ;
+
+  uint8_t l_pwm_max, l_pwm ;
+  uint8_t l_in_probe, l_out_probe ;
 
   OSCCAL = 0x56;
 
@@ -84,9 +81,9 @@ int main(void) {
   // ADC init
   ADMUX = _BV(ADLAR) ;
   ADCSRA = _BV(ADEN) | ((1<<ADPS2) | (0<<ADPS1) | (0>>ADPS0));
-  // AJOUTER INIT ADC
 
 
+/*
   l_prev = 0 ;
   for(;;) {
 
@@ -97,6 +94,34 @@ int main(void) {
    Fan_Speed(l_pwm) ;
    Serial_Debug_Send(l_pot) ; _delay_ms(1000) ;
    Serial_Debug_Send(l_pwm) ; _delay_ms(1000) ;
-//   _delay_ms(10) ;
+  }
+*/
+
+  for(;;) {
+    l_pwm_max = Pot_2_Pwm(ADC_Read(MUX_SPEED_POT)) ;
+    l_in_probe = ADC_Read(MUX_IN_PROBE) ;
+    l_out_probe = ADC_Read(MUX_OUT_PROBE) ;
+    if (l_in_probe <= l_out_probe) l_pwm = 0 ;
+      else
+        switch (l_in_probe - l_out_probe) {
+          case 1 ... 2:
+            l_pwm = PWM_MIN + (l_pwm_max - PWM_MIN) / 4 ;
+            break ;
+          case 3 ... 4:
+            l_pwm = PWM_MIN + (l_pwm_max - PWM_MIN) / 2 ;
+            break ;
+          default:
+            l_pwm = l_pwm_max ;
+        }
+    Fan_Speed(l_pwm) ;
+
+    Serial_Debug_Send(0xF0) ; _delay_ms(500) ;
+    Serial_Debug_Send(l_in_probe) ; _delay_ms(1000) ;
+    Serial_Debug_Send(0xF1) ; _delay_ms(500) ;
+    Serial_Debug_Send(l_out_probe) ; _delay_ms(1000) ;
+    Serial_Debug_Send(0xF2) ; _delay_ms(500) ;
+    Serial_Debug_Send(l_pwm_max) ; _delay_ms(1000) ;
+    Serial_Debug_Send(0xF3) ; _delay_ms(500) ;
+    Serial_Debug_Send(l_pwm) ; _delay_ms(1000) ;
   }
 }
